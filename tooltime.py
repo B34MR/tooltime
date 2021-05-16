9200341064648877603aaa918bd3e75b5fe612c1
root <root@VMWorkstation.localdomain>
2021-05-16 09:58:28 -0400

    ...

--- tooltime.py
+++ tooltime.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
 
 from concurrent import futures
+from configparser import ConfigParser
 from functools import wraps
-from utils import richard as r
-from utils import cparse as cp
-from utils import install
+from utils import arguments
 from utils import download
+from utils import install
+from utils import richard as r
+import os
 import logging
 import time
-
-
-# Confgfile values.
-GITHUB_URLS = [k for k in cp.config['github_urls']]
-BINARY_URLS = [k for k in cp.config['binary_urls']]
-PIP_PACKAGES = [k for k in cp.config['pip_packages']]
-APT_PACKAGES = [k for k in cp.config['apt_packages']]
-DEST_DIR = ''.join([k for k in cp.config['tools_dir']][0])
 
 
 def timeit(method):
@@ -51,33 +45,102 @@ def make_threaded(func, urls):
 				logging.info(f'Downloaded: {data}')
 
 
+def append_str(mystr, filepath):
+	''' 
+	Append string to file.
+	arg(s) filepath:str, mystr:str
+
+	'''
+	with open(filepath, 'a+') as f1:
+		f1.write(f'\n{mystr}')
+	
+	return mystr
+
+
+def append_lst(mylst, filename):
+	''' 
+	Append a list of strs to a file located in a user's home directory.
+	arg(s) mylst:list, filename:str
+	'''
+
+	# Custom comment.
+	comment = f'\n### Modified by: {os.path.realpath(__file__)} ###'
+	# Filepath for bashrc and bash_aliases.
+	filepath = os.path.join(os.path.expanduser('~'), f'{filename}')
+	# Append comment to file.
+	append_str(comment, filepath)
+	# Append item:str to file.
+	result = [append_str(item, filepath) for item in mylst]
+
+	return result
+
+
 @timeit
 def main():
 	''' Main func '''
 
-	# Pause, mainly used for testing.
-	r.ctrl_c()
+	# Args
+	args = arguments.parse_args()
 
-	# Downloader init.
-	dl = download.Downloader(DEST_DIR)
-	# Github-Downloads.
-	r.banner('Github Downloads')
-	with r.console.status(status=f'[status.text]Downloading') as status:
-		make_threaded(dl.get_gitrepo, GITHUB_URLS)
-	# URL-Downloads.
-	r.banner('URL Downloads')
-	with r.console.status(status=f'[status.text]Downloading') as status:
-		make_threaded(dl.get_binary, BINARY_URLS)
+	# Args - Configfile path
+	filepath = args.configfile
 
-	# Installer init.
-	installer = install.Installer()
-	# Pip Download/Install.
-	r.banner('Pip Downloads/Installs')
-	with r.console.status(status=f'[status.text]Processing...') as status:
-		results = list(map(installer.pip_install, PIP_PACKAGES))
-	# APT Download/Install.
-	r.banner('APT Downloads/Installs')
-	results = list(map(installer.apt_install, APT_PACKAGES))
+	# Set delimiter ':' for 'aliases.ini' configfile.
+	filename = os.path.basename(filepath)
+	if filename == 'aliases.ini':
+		config = ConfigParser(allow_no_value=True, delimiters=':')
+		config.optionxform = str
+		config.read(filepath)
+		# Config file values.
+		ALIASES = [k for k in config['aliases']]
+		EXPORTS = [k for k in config['bashrc']]
+		# Check if aliases are in configfile.
+		r.banner('Aliases')
+		if ALIASES:
+			results = append_lst(ALIASES, '.bash_aliases')
+			[logging.info(result) for result in results]
+		# Check if exposts are in configfile.
+		r.banner('Exports')
+		if EXPORTS:
+			results = append_lst(EXPORTS, '.bashrc')
+			[print(result) for result in results]
+
+	# Set delimiter '=' for all other configfiles.
+	else:
+		config = ConfigParser(allow_no_value=True, delimiters='=')
+		config.optionxform = str
+		config.read(filepath)
+		# Config file values.
+		DEST_DIR = ''.join([k for k in config['tools_dir']][0])
+		GITHUB_URLS = [k for k in config['github_urls']]
+		BINARY_URLS = [k for k in config['binary_urls']]
+		PIP_PACKAGES = [k for k in config['pip_packages']]
+		APT_PACKAGES = [k for k in config['apt_packages']]
+
+		# Pause, mainly used for testing.
+		r.ctrl_c()
+
+		# Downloader init.
+		dl = download.Downloader(DEST_DIR)
+		# Github-Downloads.
+		r.banner('Github Downloads')
+		with r.console.status(status=f'[status.text]Downloading') as status:
+			make_threaded(dl.get_gitrepo, GITHUB_URLS)
+		# URL-Downloads.
+		r.banner('URL Downloads')
+		with r.console.status(status=f'[status.text]Downloading') as status:
+			make_threaded(dl.get_binary, BINARY_URLS)
+
+		# Installer init.
+		installer = install.Installer()
+		# Pip Download/Install.
+		r.banner('Pip Downloads/Installs')
+		with r.console.status(status=f'[status.text]Processing...') as status:
+			results = list(map(installer.pip_install, PIP_PACKAGES))
+		# APT Download/Install.
+		r.banner('APT Downloads/Installs')
+		with r.console.status(status=f'[status.text]Processing...') as status:
+			results = list(map(installer.apt_install, APT_PACKAGES))
 
 
 if __name__ == '__main__':
