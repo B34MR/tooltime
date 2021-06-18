56f58ac2a4f8557c3869a58882b1ed169e8fa4cd
NickSanzotta <nicksanzotta@outlook.com>
2021-06-18 10:44:25 -0400

    removed legacy args

--- utils/arguments.py
+++ utils/arguments.py
@@ -23,26 +23,13 @@ def parse_args():
   Positional argument(s):
     [configfile]: Input from configuration file (defaults to './configs/internal.ini').
   """
-
-  # Optional argument(s):
-  #   [-v, --verbose]: Increase verbosity level (Include results 'None').
-  #   [-vv, --verbose --verbose]: Increase verbosity level (Include results 'None' and print results in real-time).
-  # """
   
   # Define parser
   parser = argparse.ArgumentParser(formatter_class=HelpFormatter, description='', usage=custom_usage, add_help=False)
   
   # Positional args.
   parser.add_argument('configfile', nargs="?", type=str, metavar='<configfile>', default='./configs/internal.ini', help="Input from configuration file (defaults to './configs/internal.ini')")
-  
-  # Optional argument group.
-  # optional_group = parser.add_argument_group('optional_args')
-  # optional_group.add_argument('-d', '--drop', required=False, action='store_true', default='', help='Launch with clean database.')
-
-  # Mutually Exclusive group.
-  # mutually_exclusive_group = parser.add_mutually_exclusive_group()
-  # mutually_exclusive_group.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity level (use -vv for greater effect).')
- 
+   
   # Initiate parser instance
   args = parser.parse_args()
   return args
